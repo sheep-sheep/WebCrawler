e69be0be25b4f07e430f4320b3384fe550740bf1
Yang <zhang.yang.frank@gmail.com>
2018-01-19 22:07:32 -0500

    'multiprocess'

--- url_enumerater.py
+++ url_enumerater.py
@@ -1,22 +1,43 @@
 import getpass
 import urllib
 from urllib import request
-from urllib.request import urlretrieve
+from urllib.request import urlretrieve, Request
+import concurrent.futures
 
-url = ''
+url = 'http://www.xiuren.org/xiuren/XiuRen-N00{episode}/{imgnum}.jpg'
 
-episodes = ['{0:0=2d}'.format(n) for n in range(623, 650)]
+episodes = ['{0:0=2d}'.format(n) for n in range(630, 650)]
 imgnums = ['{0:0=4d}'.format(n) for n in range(1, 70)]
 username = getpass.getuser()
 path = 'C:\\Users\\%s\\Downloads\\Images\\'%username
 
-for episode in episodes:
+
+#TODO: add a timeit decorator
+
+def getImages(episode):
+    n = 1
     for imgnum in imgnums:
-        target = url.format(episode=episode, imgnum=imgnum)
+        req = Request(url.format(episode=episode, imgnum=imgnum), headers={'User-Agent': 'Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0'})
         try:
-            r = urllib.request.urlopen(target)
-            print('checking %s'%target)
-            urlretrieve(target, path + '_'.join(target.split('/')[-2:]))
+            r = urllib.request.urlopen(req)
+            if n%20==0:
+                print('checking %s'%req.full_url)
+            response = urllib.request.urlopen(req)
+            the_page = response.read()
+            n += 1
+            with open(path + '_'.join(req.full_url.split('/')[-2:]), 'wb') as f:
+                f.write(the_page)
         except urllib.error.HTTPError as e:
-            print('Episode %s ends at %s' % (episode, imgnum))
             break
+    return 'Episode %s ends at %s' % (episode, imgnum)
+
+
+def main():
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        res = executor.map(getImages, episodes)
+        for sentence in res:
+            print(sentence)
+
+
+if __name__ == '__main__':
+    main()
